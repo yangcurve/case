01235a0f69565f65c91a6b6ae4de9584c6785d62
yangcurve <utsu0621@gmail.com>
2024-09-19 04:18:55 +0900

    .

--- src/to.ts
+++ src/to.ts
@@ -43,12 +43,12 @@ const caseMap = {
 }
 const convert =
   <C extends keyof CaseMap>(c: C) =>
-  <T>(o: T): Convert<C, T> =>
-    (typeof o === 'string' ? caseMap[c](o)
-    : o?.constructor === Object ?
-      Object.fromEntries(Object.entries(o).map(([k, v]) => [convert(c)(k), v instanceof Object ? convert(c)(v) : v]))
-    : Array.isArray(o) ? o.map(convert(c))
-    : o) as Convert<C, T>
+  <T>(t: T): Convert<C, T> =>
+    (typeof t === 'string' ? caseMap[c](t)
+    : t?.constructor === Object ?
+      Object.fromEntries(Object.entries(t).map(([k, v]) => [convert(c)(k), v instanceof Object ? convert(c)(v) : v]))
+    : Array.isArray(t) ? t.map(convert(c))
+    : t) as Convert<C, T>
 
 export namespace to {
   export type Camel<T> = Convert<'camel', T>
